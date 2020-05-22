e82c90d8883e819f3c94dedc2a84f7821e01b8ec
NguyenHoangKhang090999 <44261606+NguyenHoangKhang090999@users.noreply.github.com>
2020-05-22 10:10:57 +0700

    Update MainFrame.java

--- src/Run/MainFrame.java
+++ src/Run/MainFrame.java
@@ -17,7 +17,7 @@ public class MainFrame extends JFrame implements ActionListener {
 	JLabel label4 = new JLabel();
 	JLabel label5 = new JLabel();
 	JLabel label6 = new JLabel();
-	JLabel label7 = new JLabel();
+        JLabel label7 = new JLabel();
 	JTextField tf1 = new JTextField();
 	JTextField tf2 = new JTextField();
 	JTextField tf3 = new JTextField();
@@ -45,8 +45,8 @@ public class MainFrame extends JFrame implements ActionListener {
 		tf2.setFocusable(true);
 		tf2.setText("");
 		tf2.setEditable(true);
-
-		tf1.setBounds(140, 200, 100, 40);
+                
+                tf1.setBounds(140, 200, 100, 40);
 		tf1.setFocusable(true);
 		tf1.setText("");
 		tf1.setEditable(true);
@@ -55,28 +55,28 @@ public class MainFrame extends JFrame implements ActionListener {
 		label2.setEnabled(false);
 		label2.setText("ban kinh a: ");
 		label2.setFont(new Font("Ink Free", Font.BOLD, 20));
-
-		label1.setBounds(20, 150, 300, 40);
+		
+                label1.setBounds(20, 150, 300, 40);
 		label1.setEnabled(false);
 		label1.setText("(Theo truc hoanh) ");
 		label1.setFont(new Font("Ink Free", Font.BOLD, 15));
-
+                
 		label3.setBounds(50, 600, 150, 40);
 		label3.setEnabled(false);
 		label3.setText("Limit: a,b <= 200 ");
 		label3.setFont(new Font("Ink Free", Font.BOLD, 20));
-
-		label4.setBounds(20, 200, 150, 40);
+                
+                label4.setBounds(20, 200, 150, 40);
 		label4.setEnabled(false);
 		label4.setText("ban kinh b: ");
 		label4.setFont(new Font("Ink Free", Font.BOLD, 20));
-
-		label5.setBounds(20, 240, 300, 40);
+                
+                label5.setBounds(20, 240, 300, 40);
 		label5.setEnabled(false);
 		label5.setText("(Theo truc tung) ");
 		label5.setFont(new Font("Ink Free", Font.BOLD, 15));
-
-		label7.setBounds(20, 50, 300, 40);
+                
+                label7.setBounds(20, 50, 300, 40);
 		label7.setEnabled(false);
 		label7.setText("Tam O(0,0), ban kinh R(a,b)");
 		label7.setFont(new Font("Ink Free", Font.BOLD, 20));
@@ -89,6 +89,7 @@ public class MainFrame extends JFrame implements ActionListener {
 		bt0.setFont(new Font("Ink Free", Font.CENTER_BASELINE, 16));
 		bt0.setMnemonic(KeyEvent.VK_ENTER);
 		this.getRootPane().setDefaultButton(bt0);
+		
 
 		label6.setBounds(50, 640, 600, 40);
 		label6.setEnabled(false);
@@ -99,11 +100,11 @@ public class MainFrame extends JFrame implements ActionListener {
 		this.add(tf2);
 		this.add(label6);
 		this.add(label3);
-		this.add(label4);
-		this.add(label1);
-		this.add(label5);
-		this.add(label7);
-		this.add(tf1);
+                this.add(label4);
+                this.add(label1);
+                this.add(label5);
+                this.add(label7);
+                this.add(tf1);
 		// nut0
 		this.add(bt0);
 		bt0.setActionCommand("DRAW!");
@@ -124,13 +125,13 @@ public class MainFrame extends JFrame implements ActionListener {
 
 	public void xuLy() {
 		try {
-			int r1 = Integer.parseInt(tf2.getText());
+                        int r1 = Integer.parseInt(tf2.getText());
 			int r2 = Integer.parseInt(tf1.getText());
 			if (r2 <= 200 && r1 <= 200) {
 				sf.R2 = r2;
-				sf.R1 = r1;
+                                sf.R1 = r1;
 				System.out.print(sf.R1);
-				System.out.print(sf.R2);
+                                System.out.print(sf.R2);
 				sf.repaint();
 			} else {
 				String message = "Vui long nhap dung dinh dang! ";
@@ -144,7 +145,7 @@ public class MainFrame extends JFrame implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		if ("DRAW!".equals(e.getActionCommand())) {
+		if ("DRAW!".equals(e.getActionCommand()) ) {
 			sf.index = 0;
 			xuLy();
 		}
